6efb5efd4c2504e17cb99bc9dab73581ad8c7c88
Eyob Yibabie <eyobyibabie01@gmail.com>
2023-03-21 11:46:20 +0300

    fetching user email into chat screen

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:group_chat/constants.dart';
 import 'package:flutter/material.dart';
 class ChatScreen extends StatefulWidget {
@@ -7,6 +9,30 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth=FirebaseAuth.instance;
+  late User loggedInUser;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser(){
+   try{
+    final user= _auth.currentUser;
+    if(user != null){
+       loggedInUser = user;
+       print(loggedInUser.email);
+       print(loggedInUser.email);
+       print(loggedInUser.email);
+    }
+   }
+   catch(e){
+    print(e);
+   }
+   
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
